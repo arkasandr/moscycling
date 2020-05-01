dcc63dd249b6bec0a89baad587130fa43437f12d
ydvorzhetskiy <dvorzhetskij@mail.ru>
2020-05-01 22:29:28 +0600

    Right authentication

---
+++ src/main/java/ru/arkaleks/moscycling/security/WebSecurityConfig.java
@@ -0,0 +1,132 @@
+package ru.arkaleks.moscycling.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.RememberMeAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.rememberme.AbstractRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import ru.arkaleks.moscycling.service.AjaxAuthenticationProvider;
+import ru.arkaleks.moscycling.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import java.io.IOException;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+/**
+ * @author Alex Arkashev (arkasandr@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public static final String KEY = "posc";
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    private AjaxAuthenticationProvider ajaxProvider;
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .authenticationProvider(ajaxProvider)
+            .authenticationProvider(new RememberMeAuthenticationProvider(KEY));
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+            .antMatchers("/**/*.css")
+            .antMatchers("/**/*.js")
+            .antMatchers("/**/*.png")
+            .antMatchers("/**/*.ico");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(STATELESS)
+            .and()
+
+            .addFilterBefore(ajaxLoginFilter(), RememberMeAuthenticationFilter.class)
+
+            .authorizeRequests()
+            .antMatchers("/login*")
+            .permitAll()
+            .antMatchers("/editor.html")
+            .hasRole("ADMIN")
+            .antMatchers("/resources/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .logout()
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/login.html")
+            .and()
+            .exceptionHandling()
+            .accessDeniedPage("/error.html")
+            .and()
+
+            .rememberMe()
+            .rememberMeServices(rememberMeServices());
+    }
+
+    @Bean
+    public AbstractRememberMeServices rememberMeServices() {
+        TokenBasedRememberMeServices rememberMeServices =
+            new TokenBasedRememberMeServices(KEY, userService);
+        rememberMeServices.setAlwaysRemember(true);
+        rememberMeServices.setCookieName("remember-me-posc");
+        rememberMeServices.setTokenValiditySeconds(1209600);
+        return rememberMeServices;
+    }
+
+    @Bean
+    public AjaxLoginFilter ajaxLoginFilter() throws Exception {
+        AjaxLoginFilter filter = new AjaxLoginFilter("/login");
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setAllowSessionCreation(false);
+        filter.setAuthenticationSuccessHandler(new MyAuthenticationSuccessHandler(rememberMeServices()));
+        return filter;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
+
